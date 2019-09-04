469cabcbf3b182f775a3f997e92b3e0834c483a1
Spencer Finkel <spencerfinkel@wc-dhcp72d231.student-secure.wireless.fsu.edu>
2019-09-04 18:10:51 -0400

    initial commit

--- a1/a1_tip_calculator.py
+++ a1/a1_tip_calculator.py
@@ -3,27 +3,27 @@ print('Tip Calculator\n\n')
 # collect query
 cost = float(input('what is the cost of the meal? '))
 heads = int(input('how many people in the party? '))
-
+tip = int(input('What tip percent would you like to give? '))
 print()
 
 # variables
 tax = cost * .10
-tip = cost * .10
+#tip = cost * .10
 due = cost + tax
 total = cost + tax + tip
 split = total/heads
 
 #dictionaries to display data
-user_in_dict = {"Cost of Meal:": cost, "tax %:": 10.0, "tip %:": 10.0, "party number:": heads}
+user_in_dict = {"Cost of Meal:": cost, "tax %:": 10, "tip %:": tip, "party number:": heads}
 for x, y in user_in_dict.items():
    print(f'{x} {y}')
 print()
 print()
 print()
-user_out_dict = {'Subtotal: ': cost, 'Tax: ': tax, 'Amount Due: ': due, 'Gratuity: ': tip, 'Total: ': total, 'split: ': split}
+user_out_dict = {'Subtotal: ': cost, 'Tax:   ': tax, 'Amount Due: ': due, 'Gratuity: ': tip, 'Total: ': total, 'split: ': split}
 #for x, y in user_out_dict.items():
 #  print(y)
 
  # iteration of a formatted string to include $   
 for x, y in user_out_dict.items():
-   print(f'{x} ${y:.2f}')
+   print(f'{x} \t ${y:.2f}')
